028519a238bfa25666ee2f1d05661da245ba74b9
Nengha <68301183+Nengha-John@users.noreply.github.com>
2021-08-26 14:07:28 +0300

    Added comments

--- csv_reader.py
+++ csv_reader.py
@@ -1,5 +1,4 @@
-import csv
-print('Start')
+#The script reads through the csv file ,updates the required categories and writes the content into a new .csv file
 #Keywords that represent a particular category with a unique code
 #Make sure maneno unayoweka hapa yanaappear zaidi katika context ya mafile yako
 #If many caterogies, you can add in the data dictionary
@@ -7,18 +6,21 @@ print('Start')
 #Make sure to change the name of the old file and new file each time you run the script for multiple files
 #Make sure all the files are in the same directory as the script and execute from the terminal
 
+
+import csv
+print('Start')
 data = { 
-    'A1' : ['ujauzito','mimba','umeshajifungua','kujifungua','jifungua','mimba','mjamzito','mama na mtoto'],
-    'A2':['corona','covid','covid-19','kifua','mafua makali'],
-    'A3':['chanjo','sindano'],
-    'A4' : ['mtoto','kuharisha','chanjo','kuhara','utapiamlo'],
-    'A5': ['uzito','nyonya','nyonyesha','mtoto mchanga','maziwa','urefu','kipimo'],
-    'A6': ['mtoto','kijana','uzazi','balehe','kubalehe','elimu ya uzazi','msichana','mvulana'],
-    'A7': ['afya','kliniki','dawa','shinikizo la damu','damu','presha','kupima','kipimo']
+    'A1' : ['Maneno ambayo yako related na category husika'],
+    'A2': ['Maneno ambayo yako related na category husika'],
+    'A3': ['Maneno ambayo yako related na category husika'],
+    'A4' :  ['Maneno ambayo yako related na category husika'],
+    'A5':  ['Maneno ambayo yako related na category husika'],
+    'A6':  ['Maneno ambayo yako related na category husika'],
+    'A7':  ['Maneno ambayo yako related na category husika']
     }
 
 #Open the file that has the particular data
-with open('user27_Msgs_File_4.csv', newline='') as csv_file:
+with open('name_of_file.csv', newline='') as csv_file:
     file = csv.reader(csv_file,delimiter=',')
     #Loop through each row in the file
     for row in file:
@@ -34,7 +36,7 @@ with open('user27_Msgs_File_4.csv', newline='') as csv_file:
                     #Store the keys in a single list
                         ids.append(key)
         #Create a copy of the csv file that will store the filled version
-        with open('user27_Msgs_File_4(filled).csv',mode='a') as csvFileR:
+        with open('name_of_new_csv_file_(filled).csv',mode='a') as csvFileR:
             write = csv.writer(csvFileR,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             for id in ids:
                 if id not in row[0]:
